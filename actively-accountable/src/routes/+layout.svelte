8963710b9372306f255b1caaf4e8f1aaa24f4837
AngelaSlattery <122385006+AngelaSlattery@users.noreply.github.com>
2024-12-01 13:38:59 -0600

    Updated Profile Look
    Added Weight, Desired Weight, Goal Intake, Daily Activity, Profile Picture to Profile Page.  Created features that allows users to be able to edit their profile.  I also added the lime image by Actively Accountable Header.

--- actively-accountable/src/routes/+layout.svelte
+++ actively-accountable/src/routes/+layout.svelte
@@ -3,8 +3,17 @@
 </script>
 
 <nav class="bg-gray-800 p-4 text-white">
-	<div class="mx-auto flex max-w-7xl justify-between">
-		<a href="/" class="text-lg font-bold">Actively Accountable</a>
+	<div class="mx-auto flex max-w-7xl items-center justify-between">
+		<div class="flex items-center space-x-2">
+			<img 
+				src="https://i5.walmartimages.com/asr/12314833-2e54-4739-94a2-7db45b63109d.16ff07e3c111df9be4158853c2e505ef.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF" 
+				alt="Lime" 
+				class="h-8 w-8 rounded-full"
+			/>
+			<a href="/" class="text-lg font-bold">Actively Accountable</a>
+		</div>
+
+		<!-- Navigation Links -->
 		<div>
 			<a href="/login" class="mr-4 hover:underline">Login</a>
 			<a href="/profile" class="hover:underline">Profile</a>
